1e6ec7e3b21e4c15548d87202d05e2ac5fc591aa
jzcz38 <jzcz38@bosera.com>
2020-07-11 16:57:44 +0800

    优化wechat自动浏览js

--- js/wechat/index.js
+++ js/wechat/index.js
@@ -163,18 +163,50 @@ iContent.addEventListener("click",function (e) {
         hChild += iChild[i].offsetHeight
     }
     if(hChild > hParent){
-        if (timer) clearInterval(timer)
-        timer = setInterval(()=>{
-            if (iContent.scrollTop + hParent < hChild){
-                // iContent.scrollTop += h;
-                iContent.scrollTop += 1;
-            }else{
-                iContent.scrollTop = 0;
-            }
-
-        },10)
+        // if (timer) clearInterval(timer)
+        // timer = setInterval(()=>{
+        //     if (iContent.scrollTop + hParent < hChild){
+        //         // iContent.scrollTop += h;
+        //         iContent.scrollTop += 1;
+        //     }else if(iContent.scrollTop + hParent == hChild){
+        //         // iContent.scrollTop -= 1;
+        //         clearInterval(timer)
+        //         setTimeout(()=>{
+        //             // iContent.scrollTop = 0;
+        //             timer = setInterval(()=>{
+        //                 iContent.scrollTop -= 1;
+        //                 if (iContent.scrollTop == 0){
+        //                     clearInterval(timer)
+        //                 }
+        //             },10)
+        //         },3000)
+        //     }
+        // },10)
+        cycle(hParent,hChild)
     }
 })
+
+function cycle(hP,hC){
+    if (timer) clearInterval(timer)
+    timer = setInterval(()=>{
+        if (iContent.scrollTop + hP < hC){
+            iContent.scrollTop += 1;
+        }else if(iContent.scrollTop + hP == hC){
+            clearInterval(timer)
+            setTimeout(()=>{
+                timer = setInterval(()=>{
+                    iContent.scrollTop -= 1;
+                    if (iContent.scrollTop == 0){
+                        clearInterval(timer)
+                        setTimeout(()=>{
+                            cycle(hP,hC)
+                        },3000)
+                    }
+                },10)
+            },3000)
+        }
+    },10)
+}
 
 footer.addEventListener("click",function (e) {
     clearInterval(timer)
