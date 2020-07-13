2048598307f63b6475c30b5c6eb29cda8cae2e0a
jzcz38 <jzcz38@bosera.com>
2020-07-13 09:39:12 +0800

    聊天页面ui的编辑 暂未写功能

--- js/wechat/wechat.js
+++ js/wechat/wechat.js
@@ -2,3 +2,13 @@ var wechat = document.getElementsByClassName('wechat')[0]
 wechat.addEventListener('click',function (e) {
     console.log('dyk',window.location.href.split("?")[1])
 })
+
+var time = document.getElementById('time');
+var input = document.getElementById('input');
+var send = document.getElementById('send');
+
+send.addEventListener('click',function (e) {
+    console.log(input.value+'1111')
+})
+
+time.innerHTML = nowTime()
