ae9f3f5358e0ebe460b20108d8c91815e73df88f
jzcz38 <jzcz38@bosera.com>
2020-07-13 19:02:23 +0800

    小游戏的ui设计

---
+++ js/found/game.js
@@ -0,0 +1,11 @@
+var child = document.getElementById('child');
+var left = document.getElementById('left');
+var left = document.getElementById('left');
+var left = document.getElementById('left');
+var left = document.getElementById('left');
+var t = 0;
+
+// setInterval(()=>{
+//     child.style.top = t + 'px';
+//     t ++;
+// },100)
